092dda8bd2690cde40c87532ce78777cd544f8ac
Joseph <tapang@hawaii.edu>
2015-09-21 22:40:31 -1000

    Changed getClasses
    Misunderstood reason for function, previously was searching for element
    tag in function. Changed function to just work with an element given to
    it, no searching.

--- HW/A07/myScript.js
+++ HW/A07/myScript.js
@@ -3,23 +3,30 @@
  */
 
 function getClasses(elem) {
-  //makes a nodeList of element objects
-  var myNodeList = document.getElementsByTagName(elem);
+//find class="
+ var elemsClasses = elem.className;
+  //split elemsClasses by " " into an array
+  var arrayOfClasses = elemsClasses.split(" ");
+  //add elements of array to new string with []'s and ""s
+  var newString;
+  for(i=0; i<arrayOfClasses.length;i++){
 
-  /*Code below prints out classes for each element on a line*/
-  //document.getElementById("result").innerHTML = myNodeList[0].className + "<br>" +
-  //myNodeList[1].className;
-  //first string of classes separated by ' '
-  var stringOfClasses = myNodeList[0].className;
-  //this will be the array of classes from the split string of classes
-  var classArray = stringOfClasses.split(" ");
-  //print out string of array of classes as class,class
-  //need to override toString to add []'s and ""'s
-  document.getElementById("result").innerHTML = classArray.toString();
+    if(i==0){
+      newString = "[" + "\"" + arrayOfClasses[i] + "\", ";
+    }
+    else if(i!=0 && i!=arrayOfClasses.length-1){
+      newString = newString + "\"" + arrayOfClasses[i] + "\", ";
+    }
+    else if(i==arrayOfClasses.length-1){
+      newString = newString + "\"" + arrayOfClasses[i] + "\"" + "]";
+    }
+  }
+
+  document.getElementById("result").innerHTML = newString;
 }
 
 function addClass(elem, className){
-  
+
 }
 //to be done later
 function validateForm(){
