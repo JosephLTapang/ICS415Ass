7c1a654a4fc2882dd7139b98932b737c8a82f40f
Joseph <tapang@hawaii.edu>
2015-09-21 21:31:18 -1000

    classes print out
    Classes print out separated by ,'s. Had some problems with node Lists
    until looking at element functions.

--- HW/A07/myScript.js
+++ HW/A07/myScript.js
@@ -2,12 +2,27 @@
  * Created by StickandJab on 9/18/2015.
  */
 
-function getClasses(elem){
+function getClasses(elem) {
   //makes a nodeList of element objects
-  var	myNodeList = document.getElementsByTagName(elem);
+  var myNodeList = document.getElementsByTagName(elem);
 
-  document.getElementById("result").innerHTML = myNodeList[0].className + "<br>" + myNodeList[1].className;
+  /*Code below prints out classes for each element on a line*/
+  //document.getElementById("result").innerHTML = myNodeList[0].className + "<br>" +
+  //myNodeList[1].className;
+  //first string of classes separated by ' '
+  var stringOfClasses = myNodeList[0].className;
+  //this will be the array of classes from the split string of classes
+  var classArray = stringOfClasses.split(" ");
+  //print out string of array of classes as class,class
+  //need to override toString to add []'s and ""'s
+  document.getElementById("result").innerHTML = classArray.toString();
+}
 
+function addClass(elem, className){
+  
+}
+//to be done later
+function validateForm(){
 
 }
 
