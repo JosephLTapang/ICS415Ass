8583c26e2014186242d7034fdf1abe1faac6aea1
Joseph <tapang@hawaii.edu>
2015-09-21 23:16:15 -1000

    addClass
    Can add classes to elements who have class attribute and those that do
    not. Also realized the added classes show up in inspect element not in
    the actual html file.

--- HW/A07/myScript.js
+++ HW/A07/myScript.js
@@ -7,12 +7,16 @@ function getClasses(elem) {
  var elemsClasses = elem.className;
   //split elemsClasses by " " into an array
   var arrayOfClasses = elemsClasses.split(" ");
+
   //add elements of array to new string with []'s and ""s
   var newString;
   for(i=0; i<arrayOfClasses.length;i++){
 
-    if(i==0){
+    if(i==0 && (arrayOfClasses.length != 1)){
       newString = "[" + "\"" + arrayOfClasses[i] + "\", ";
+    }
+    else if(i==0 && (arrayOfClasses.length == 1)){
+      newString = "[" + "\"" + arrayOfClasses[i] + "\"]";
     }
     else if(i!=0 && i!=arrayOfClasses.length-1){
       newString = newString + "\"" + arrayOfClasses[i] + "\", ";
@@ -21,14 +25,21 @@ function getClasses(elem) {
       newString = newString + "\"" + arrayOfClasses[i] + "\"" + "]";
     }
   }
-
+  //print results in div with id "result"
   document.getElementById("result").innerHTML = newString;
 }
 
 function addClass(elem, className){
-
+  //if no class then add class attribute with className
+  if(elem.hasAttribute("class") == false){
+    elem.setAttribute("class", className);
+  }
+  //add className to end of classes preceded by a space
+  else{
+    elem.className += " " + className;
+  }
 }
-//to be done later
+
 function validateForm(){
 
 }
