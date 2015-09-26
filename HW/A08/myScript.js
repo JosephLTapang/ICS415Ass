770868b21f7237a2cecc4102a869bbe3ef73b6be
Joseph <tapang@hawaii.edu>
2015-09-25 20:43:01 -1000

    Basic layout and toggle
    Basic page layout set. Mock questions with answers can be toggled. Need
    to add + - to signify expanded or contracted faq

---
+++ HW/A08/myScript.js
@@ -0,0 +1,45 @@
+/**
+ * Created by StickandJab on 9/25/2015.
+ */
+
+//JQuery functions go here
+function answerOne() {
+  $(document).ready(function () {
+    $("#one").click(function () {
+      $("#answer1").toggle();
+    });
+  });
+}
+
+function answerTwo() {
+  $(document).ready(function () {
+    $("#two").click(function () {
+      $("#answer2").toggle();
+    });
+  });
+}
+
+function answerThree() {
+  $(document).ready(function () {
+    $("#three").click(function () {
+      $("#answer3").toggle();
+    });
+  });
+}
+
+function answerFour() {
+  $(document).ready(function () {
+    $("#four").click(function () {
+      $("#answer4").toggle();
+    });
+  });
+}
+
+function answerFive() {
+  $(document).ready(function () {
+    $("#five").click(function () {
+      $("#answer5").toggle();
+    });
+  });
+}
+
